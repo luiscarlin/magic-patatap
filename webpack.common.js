0a76cb945703640afcfa3e7610bc5e8d509cb397
Luis Carlin <luiscarlin@gmail.com>
2018-09-08 09:18:43 -0400

    Remove copy plugin. Favicon can be added by using html plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,6 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-
-const copyFiles = [
-  { from: './public/favicon.ico', to: './' }
-]
 
 module.exports = {
   entry: {
@@ -14,10 +9,9 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    }),
-    new CopyWebpackPlugin(copyFiles)
-
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
   ],
   output: {
     filename: '[name].bundle.js',
