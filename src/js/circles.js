690c73eb71293bcdb59d44fb60bff0b959ff4cec
Luis Carlin <luiscarlin@gmail.com>
2018-09-15 10:51:17 -0400

    Refactor and add initial message

--- src/js/circles.js
+++ src/js/circles.js
@@ -3,51 +3,61 @@ import notes from './notes'
 import io from 'socket.io-client'
 
 const socket = io()
-
-socket.on('users', count => {
-  console.log('number of users ', count)
-})
-
 let circles = []
+
+const initializeSocketio = () => {
+  socket.on('users', count => {
+    console.log('number of users ', count)
+  })
+}
 
 const initializePaper = () => {
   let canvas = window.document.getElementById('circleCanvas')
   paper.setup(canvas)
-  paper.view.onFrame = onFrame
+  paper.view.onFrame = () => {
+    circles.forEach((circle, index, object) => {
+      circle.fillColor.hue += 1
+      circle.scale(0.9)
+
+      if (circle.area < 1) {
+        circle.remove()
+        object.splice(index, 1)
+      }
+    })
+  }
 }
 
-const onFrame = () => {
-  circles.forEach((circle, index, object) => {
-    circle.fillColor.hue += 1
-    circle.scale(0.9)
+const removeWelcomeHandler = () => {
+  console.log('hello')
+  let welcome = window.document.getElementById('welcome')
+  welcome.classList.add('hide')
+  window.removeEventListener('keydown', removeWelcomeHandler)
+}
 
-    if (circle.area < 1) {
-      circle.remove()
-      object.splice(index, 1)
-    }
-  })
+const attemptPlayNoteHandler = (event) => {
+  if (notes[event.key]) {
+    const maxPoint = new paper.Point(
+      paper.view.size.width,
+      paper.view.size.height
+    )
+    const randomPoint = paper.Point.random()
+    const point = maxPoint.multiply(randomPoint)
+
+    const newCircle = new paper.Path.Circle(point, 500)
+    newCircle.fillColor = notes[event.key].color
+    notes[event.key].sound.play()
+
+    circles.push(newCircle)
+  }
 }
 
 const addKeyEvents = () => {
-  window.addEventListener('keydown', event => {
-    if (notes[event.key]) {
-      const maxPoint = new paper.Point(
-        paper.view.size.width,
-        paper.view.size.height
-      )
-      const randomPoint = paper.Point.random()
-      const point = maxPoint.multiply(randomPoint)
-
-      const newCircle = new paper.Path.Circle(point, 500)
-      newCircle.fillColor = notes[event.key].color
-      notes[event.key].sound.play()
-
-      circles.push(newCircle)
-    }
-  })
+  window.addEventListener('keydown', removeWelcomeHandler)
+  window.addEventListener('keydown', attemptPlayNoteHandler)
 }
 
 export default () => {
+  initializeSocketio()
   initializePaper()
   addKeyEvents()
 }
